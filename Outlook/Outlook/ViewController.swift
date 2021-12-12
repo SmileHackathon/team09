f1fd7f8ccd5cc614d6dd4e5ff55c10c645c6c921
佐々木翔太 <sasakishouta@sasakishoutanoiMac.local>
2021-12-12 20:45:23 +0900

    add xcodefile

---
+++ Outlook/Outlook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Outlook
+//
+//  Created by 佐々木翔太 on 2021/12/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
